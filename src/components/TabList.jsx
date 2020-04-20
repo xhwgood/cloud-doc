1b1cf23d555cb2c5b2baebd6ca11d35d67b9874d
xhwgood <734824565@qq.com>
2020-04-20 08:01:24 +0800

    简化函数；上传图片

--- src/components/TabList.jsx
+++ src/components/TabList.jsx
@@ -5,46 +5,44 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import './TabList.scss'
 
-const TabList = ({ files, activeId, unsaveIds, onTabClick, onCloseTab }) => {
-	return (
-		<ul className='nav nav-pills tablist-components'>
-			{files.map(file => {
-				const withUnsavedMark = unsaveIds.includes(file.id)
-				const fClassName = classNames({
-					'nav-link': true,
-					active: file.id === activeId,
-					withUnsaved: withUnsavedMark
-				})
-				return (
-					<li className='nav-item' key={file.id}>
-						<a
+const TabList = ({ files, activeId, unsaveIds, onTabClick, onCloseTab }) => (
+	<ul className='nav nav-pills tablist-components'>
+		{files.map(file => {
+			const withUnsavedMark = unsaveIds.includes(file.id)
+			const fClassName = classNames({
+				'nav-link': true,
+				active: file.id === activeId,
+				withUnsaved: withUnsavedMark
+			})
+			return (
+				<li className='nav-item' key={file.id}>
+					<a
+						onClick={e => {
+							e.preventDefault()
+							onTabClick(file.id)
+						}}
+						className={fClassName}
+						href='#'
+					>
+						{file.title}
+						<span
 							onClick={e => {
-								e.preventDefault()
-								onTabClick(file.id)
+								e.stopPropagation()
+								onCloseTab(file.id)
 							}}
-							className={fClassName}
-							href='#'
+							className='ml-2 close-icon'
 						>
-							{file.title}
-							<span
-								onClick={e => {
-									e.stopPropagation()
-									onCloseTab(file.id)
-								}}
-								className='ml-2 close-icon'
-							>
-								<FontAwesomeIcon icon={faTimes} />
-							</span>
-							{withUnsavedMark && (
-								<span className='rounded-circle unsaved-icon ml-2'></span>
-							)}
-						</a>
-					</li>
-				)
-			})}
-		</ul>
-	)
-}
+							<FontAwesomeIcon icon={faTimes} />
+						</span>
+						{withUnsavedMark && (
+							<span className='rounded-circle unsaved-icon ml-2'></span>
+						)}
+					</a>
+				</li>
+			)
+		})}
+	</ul>
+)
 
 TabList.propTypes = {
 	files: PropTypes.array,
